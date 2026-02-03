512da2b027eec5082f556274c39e1b7cd63123f4
sahil pandey <sahil.pandeyy023@gmail.com>
2026-02-03 23:22:45 +0530

    Fix: Replace visible keys with env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB-DVKR_6PgidEt7smqiTUXpbLvBuq4fBg",
-    authDomain: "landingpage-10420.firebaseapp.com",
-    projectId: "landingpage-10420",
-    storageBucket: "landingpage-10420.firebasestorage.app",
-    messagingSenderId: "802825132220",
-    appId: "1:802825132220:web:ea1843a054bcafa2f858b9",
-    measurementId: "G-3RVDH1009D"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
